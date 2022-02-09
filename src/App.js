10db03bd2176456229496dd09ccf5004551658ed
ayushhagarwal <ayushgrwl365@gmail.com>
2022-02-09 23:32:49 +0530

    navbar component complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import Navbar from "./Components/Navbar/Navbar";
+
 const App = () => {
   return (
-    <div className="App">
+    <div className="App h-screen bg-blue-300 ">
+      <Navbar menu_1={"About"} menu_2={"Login"} />
       <h1 className="text-2xl text-center">
         Hello from react and tailwind project
       </h1>
