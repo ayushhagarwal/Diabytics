b739902cd330127629f3617d15e6116863b04a24
ayushhagarwal <ayushgrwl365@gmail.com>
2022-02-15 19:23:09 +0530

    form UI improvement complete

--- src/Components/AddData/AddData.js
+++ src/Components/AddData/AddData.js
@@ -18,31 +18,42 @@ const AddData = ({ add }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <input
-          type="date"
-          placeholder="Date of test"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
+    <div className="'h-screen flex bg-gray-bg1'">
+      <div className="w-full max-w-md m-auto bg-blue-100 rounded-lg border border-primaryBorder shadow-default py-10 px-16">
+        <form onSubmit={onSubmit}>
+          <div>
+            <input
+              type="date"
+              placeholder="Date of test"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="PPBS "
+              onChange={(e) => setPpbs(e.target.value)}
+              className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="FBS"
+              onChange={(e) => setFbs(e.target.value)}
+              className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
+            />
+          </div>
+          <input
+            type="submit"
+            value="Submit Results"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded"
+          />
+        </form>
       </div>
-      <div>
-        <input
-          type="number"
-          placeholder="PPBS "
-          onChange={(e) => setPpbs(e.target.value)}
-        />
-      </div>
-      <div>
-        <input
-          type="number"
-          placeholder="FBS"
-          onChange={(e) => setFbs(e.target.value)}
-        />
-      </div>
-      <input type="submit" value="Submit Results" />
-    </form>
+    </div>
   );
 };
 
